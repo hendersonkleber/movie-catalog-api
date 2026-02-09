b1d4fd238a9cf36d1f99facab6a64f74d6800993
hendersonkleber <hendersonkleber@gmail.com>
2026-02-09 14:28:22 -0300

    initial commit

---
+++ src/main/java/br/com/hendersonkleber/movie_catalog/MovieCatalogApplication.java
@@ -0,0 +1,11 @@
+package br.com.hendersonkleber.movie_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/hendersonkleber/movie_catalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hendersonkleber.movie_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
