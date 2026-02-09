3667f2acb5c87593b6d6b59ce9c44c4065c8aaf8
hendersonkleber <hendersonkleber@gmail.com>
2026-02-09 16:07:28 -0300

    feat: add movie entity

---
+++ src/main/java/br/com/hendersonkleber/movie_catalog/domain/Movie.java
@@ -0,0 +1,117 @@
+package br.com.hendersonkleber.movie_catalog.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+
+    @Column(name = "title", length = 200)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "genre", length = 60)
+    private String genre;
+
+    @Column(name = "release_year", length = 4)
+    private String releaseYear;
+
+    @Column(name = "duration")
+    private double duration;
+
+    public Movie() {
+    }
+
+    public Movie(UUID id, LocalDateTime createAt, LocalDateTime updateAt, String title, String description, String genre, String releaseYear, double duration) {
+        this.id = id;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.duration = duration;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+}
