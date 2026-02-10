aa4bd4baf19db330a0ecbe825191da735c4072d3
hendersonkleber <hendersonkleber@gmail.com>
2026-02-10 09:59:24 -0300

    feat: add custom exceptions

---
+++ src/main/java/br/com/hendersonkleber/movie_catalog/exception/ApiException.java
@@ -0,0 +1,27 @@
+package br.com.hendersonkleber.movie_catalog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+import java.time.LocalDateTime;
+
+public abstract class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public ProblemDetail toProblemDetail() {
+        var status = HttpStatus.INTERNAL_SERVER_ERROR;
+        var problem = ProblemDetail.forStatus(status);
+
+        problem.setTitle("API exception");
+        problem.setDetail("Contact support");
+        problem.setProperty("timestamp", LocalDateTime.now());
+
+        return problem;
+    }
+}
---
+++ src/main/java/br/com/hendersonkleber/movie_catalog/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package br.com.hendersonkleber.movie_catalog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+import java.time.LocalDateTime;
+
+public class ResourceNotFoundException extends ApiException {
+    private final String message;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var status = HttpStatus.NOT_FOUND;
+        var problem = ProblemDetail.forStatus(status);
+
+        problem.setTitle("API exception");
+        problem.setDetail(this.message);
+        problem.setProperty("timestamp", LocalDateTime.now());
+
+        return problem;
+    }
+}
